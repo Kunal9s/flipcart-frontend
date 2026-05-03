5ef3aa69da6babf3a06eaf5559ae3fc60f78158e
Kunal9s <kunal8x@gmail.com>
2026-05-04 02:08:15 +0530

    frontend is fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,49 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import Banner from "./Banner";
 import NavBar from "./NavBar";
-import Slide from './Slide';
+import Slide from "./Slide";
 
-import { Box, styled } from '@mui/material';
-import { getProducts } from '../../redux/actions/productActions';
-import { useDispatch, useSelector } from 'react-redux';
-import MidSlide from './MidSlide';
-import MidSection from './MidSection';
+import { Box, styled } from "@mui/material";
+import { getProducts } from "../../redux/actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
+import MidSlide from "./MidSlide";
+import MidSection from "./MidSection";
 
 const Component = styled(Box)`
-    padding: 20px 10px;
-    // background: #F2F2F2;
-`
+  padding: 20px 10px;
+  // background: #F2F2F2;
+`;
 
 const Home = () => {
+  const productState = useSelector((state) => state.getProducts);
+  const { products } = productState;
+  console.log(products);
 
-    const productState = useSelector(state => state.getProducts);
-    const { products } = productState;
-    console.log(products);
-    
+  useEffect(() => {
+    const fetchData = () => {
+      dispatch(getProducts());
+    };
 
-    const dispatch = useDispatch();
+    fetchData();
 
-    useEffect(() => {
-        dispatch(getProducts())
-    }, [dispatch])
+    const interval = setInterval(fetchData, 10000); 
 
-    return (
-        <>
-          <NavBar />
-          <Component>
-             <Banner />
-             <MidSlide products={products} title="Deal of the Day" timer={true} />
-             <MidSection />
-             <Slide products={products} title="Discounts for You" timer={false} />
-             <Slide products={products} title="Suggesting Items" timer={false} />
-             <Slide products={products} title="Top Selection" timer={false} />
-             <Slide products={products} title="Recommended Items" timer={false} />
-             <Slide products={products} title="Trending Offers" timer={false} />
-             <Slide products={products} title="Season's top picks" timer={false} />
-             <Slide products={products} title="Top Deals on Accessories" timer={false} />
-          </Component>
-        </>
-    )
-}
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
+  return (
+    <>
+      <NavBar />
+      <Component>
+        <Banner />
+        <MidSlide products={products} title="Deal of the Day" timer={true} />
+        <MidSection />
+        <Slide products={products} title="Discounts for You" timer={false} />
+        <Slide products={products} title="Suggesting Items" timer={false} />
+        <Slide products={products} title="Top Selection" timer={false} />
+        <Slide products={products} title="Recommended Items" timer={false} />
+        <Slide products={products} title="Trending Offers" timer={false} />
+        <Slide products={products} title="Season's top picks" timer={false} />
+        <Slide
+          products={products}
+          title="Top Deals on Accessories"
+          timer={false}
+        />
+      </Component>
+    </>
+  );
+};
 
 export default Home;
